c8fe0e7ec1244b421a775b39e0ddc56c40a2d0b4
Anders Schmidt Hansen <me@andersschmidt.com>
2018-07-18 18:53:14 +0200

    started work on Terrain

---
+++ js/core/gfx/terrain.js
@@ -0,0 +1,5 @@
+export default class Terrain {
+  constructor(sources) {
+    
+  }
+}
--- js/game.js
+++ js/game.js
@@ -4,19 +4,18 @@ import Sprite from "./core/gfx/sprite.js";
 export default class Game {
     constructor() {
         this.client = new GameClient();
-        this.logo = Sprite.fromUrl("/img/logo.png");
+        this.terrains = {
+            grass: Sprite.fromUrl('/assets/sprites/tiles/grass.png')
+        }
     }
 
-    draw(gfx, elapsed) {
-        gfx.clear("#333");
+    drawTerrain(gfx) {
+        const grass = this.terrains.grass
+        if (grass.ready) return gfx.drawSprite(grass, 0, 0)
+    }
 
-        if (this.logo.ready) {
-            this.logo.x = gfx.width / 2 - this.logo.width / 2;
-            this.logo.y = 20;
-            gfx.drawSprite(this.logo);
-        } else {
-            gfx.drawString("Loading logo...", gfx.width / 2, 20, "white");
-        }
+    async draw(gfx, elapsed) {
+        await this.drawTerrain(gfx)
     }
 
     update(elapsed) {
